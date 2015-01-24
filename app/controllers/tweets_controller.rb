248b16b8e3e008d098e7a320e94ad7da6b2a5f4e
Kevin Gleason <kaagleason@gmail.com>
2015-01-24 14:44:25 -0500

    Added bootstrap

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,18 @@
 class TweetsController < ApplicationController
 	def new
 		@tweet = Tweet.new
-		
+	end
+
+	def create
+		@tweet = Tweet.create(tweet_params)
+		flash.now[:success] = "Tweet Created"
+		render 'new'
+	end
+
+	private
+
+	def tweet_params
+		# Tweet params only permit content.
+		params.require(:tweet).permit(:content)
 	end
 end
