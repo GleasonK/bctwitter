7d243a9e930ae218bf204b61e67f03eae2bd12fc
Kevin Gleason <kaagleason@gmail.com>
2015-01-25 13:38:17 -0500

    Added lots

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,28 @@
 class TweetsController < ApplicationController
+
+	before_action :authenticate_user! # ! means raise error if not working
+
 	def new
 		@tweet = Tweet.new
+		@tweets = current_user.tweets
 	end
 
 	def create
-		@tweet = Tweet.create(tweet_params)
-		flash.now[:success] = "Tweet Created"
-		render 'new'
-	end
+# -		@tweet = Tweet.create(tweet_params)
+
+		@tweet = Tweet.new(tweet_params) # creates new instance of Tweet
+		@tweet.user = current_user
+		@tweet.save # saves it to the database
+		@tweets = current_user.tweets
+
+ 		flash.now[:success] = "Tweet Created"
+ 		render 'new'
+ 	end
+
+ 	def index
+ 		# All tweets but not our own.
+ 		@tweets = Tweet.all.reject{ |tweet|  tweet.user == current_user }
+ 	end
 
 	private
 
